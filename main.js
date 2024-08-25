a6af555862ddcf069e799dd0c781b14794cc5fee
GustaLeao <leao.gustavo075@gmail.com>
2024-08-25 17:40:55 -0300

    add dessert to cart working html semantic tag modified class names modified

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 const container = document.getElementById("desserts_container");
 const btns = document.getElementsByClassName("dessert_btn");
-let btnArr = [];
 const wrappersArray = document.getElementsByClassName("btn_wrapper");
 const cartQty = document.getElementById("cart_title");
 const cartList = document.getElementById("cart_list");
 const icons = {
   addToCart: `<svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" fill="none" viewBox="0 0 21 20"><g fill="#C73B0F" clip-path="url(#a)"><path d="M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z"/><path d="M11.584 3.75v-2.5h-1.25v2.5h-2.5V5h2.5v2.5h1.25V5h2.5V3.75h-2.5Z"/></g><defs><clipPath id="a"><path fill="#fff" d="M.333 0h20v20h-20z"/></clipPath></defs></svg>`,
   carbonNeutral: `<svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" fill="none" viewBox="0 0 21 20"><path fill="#1EA575" d="M8 18.75H6.125V17.5H8V9.729L5.803 8.41l.644-1.072 2.196 1.318a1.256 1.256 0 0 1 .607 1.072V17.5A1.25 1.25 0 0 1 8 18.75Z"/><path fill="#1EA575" d="M14.25 18.75h-1.875a1.25 1.25 0 0 1-1.25-1.25v-6.875h3.75a2.498 2.498 0 0 0 2.488-2.747 2.594 2.594 0 0 0-2.622-2.253h-.99l-.11-.487C13.283 3.56 11.769 2.5 9.875 2.5a3.762 3.762 0 0 0-3.4 2.179l-.194.417-.54-.072A1.876 1.876 0 0 0 5.5 5a2.5 2.5 0 1 0 0 5v1.25a3.75 3.75 0 0 1 0-7.5h.05a5.019 5.019 0 0 1 4.325-2.5c2.3 0 4.182 1.236 4.845 3.125h.02a3.852 3.852 0 0 1 3.868 3.384 3.75 3.75 0 0 1-3.733 4.116h-2.5V17.5h1.875v1.25Z"/></svg>`,
-  decrementQty: `<svg xmlns="http://www.w3.org/2000/svg" width="14" height="16" viewBox="0 0 24 16" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-minus"><path d="M5 12h14"/></svg>`,
-  incrementQty: `<svg xmlns="http://www.w3.org/2000/svg" width="14" height="16" viewBox="0 0 24 18" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus"><path d="M5 12h14"/><path d="M12 5v14"/></svg>`,
+  minus: `<svg xmlns="http://www.w3.org/2000/svg" width="14" height="16" viewBox="0 0 24 16" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-minus"><path d="M5 12h14"/></svg>`,
+  plus: `<svg xmlns="http://www.w3.org/2000/svg" width="14" height="16" viewBox="0 0 24 18" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus"><path d="M5 12h14"/><path d="M12 5v14"/></svg>`,
   orderConfirmed: `<svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M21 32.121L13.5 24.6195L15.6195 22.5L21 27.879L32.3775 16.5L34.5 18.6225L21 32.121Z" fill="#1EA575"/>
 <path d="M24 3C19.8466 3 15.7865 4.23163 12.333 6.53914C8.8796 8.84665 6.18798 12.1264 4.59854 15.9636C3.0091 19.8009 2.59323 24.0233 3.40352 28.0969C4.21381 32.1705 6.21386 35.9123 9.15077 38.8492C12.0877 41.7861 15.8295 43.7862 19.9031 44.5965C23.9767 45.4068 28.1991 44.9909 32.0364 43.4015C35.8736 41.812 39.1534 39.1204 41.4609 35.667C43.7684 32.2135 45 28.1534 45 24C45 18.4305 42.7875 13.089 38.8493 9.15076C34.911 5.21249 29.5696 3 24 3ZM24 42C20.4399 42 16.9598 40.9443 13.9997 38.9665C11.0397 36.9886 8.73256 34.1774 7.37018 30.8883C6.0078 27.5992 5.65134 23.98 6.34587 20.4884C7.04041 16.9967 8.75474 13.7894 11.2721 11.2721C13.7894 8.75473 16.9967 7.0404 20.4884 6.34587C23.98 5.65133 27.5992 6.00779 30.8883 7.37017C34.1774 8.73255 36.9886 11.0397 38.9665 13.9997C40.9443 16.9598 42 20.4399 42 24C42 28.7739 40.1036 33.3523 36.7279 36.7279C33.3523 40.1036 28.7739 42 24 42Z" fill="#1EA575"/>
@@ -31,7 +30,12 @@ const icons = {
 let dataList = [];
 let dessertInCart = [];
 let totalInCart = 0;
-const getDataTo = async (f) => {
+let btnArr = [];
+for (let btn of btns) {
+  btnArr.push(btn);
+}
+
+const getDataTo = async (f, i) => {
   try {
     const res = await fetch("data.json");
     const resTwo = await fetch("svg-data.json");
@@ -42,7 +46,12 @@ const getDataTo = async (f) => {
 
     const data = await res.json();
 
-    f(data);
+    if (f === populateContainer) {
+      f(data);
+    }
+    if (f === modifyCartList) {
+      f(data, i);
+    }
   } catch (error) {
     console.error("error fetching data:", error);
   }
@@ -54,16 +63,14 @@ const populateContainer = (dataArr) => {
     return;
   }
 
-  dataList = [];
-  for (data of dataArr) {
+  for (let data of dataArr) {
     dataList.push(data);
     const { image, name, category, price } = data;
-
     container.innerHTML += `
             <div class='dessert'> 
                 <img class='dessert_img' src='${image.desktop}'/>
                 <div class='btn_wrapper'>
-                  <button class='dessert_btn' onclick=incrementQty(event)>  </button>
+                  <button class='dessert_btn' onclick=verifyCart(event)>  </button>
                   ${icons.addToCart} 
                   <p>Add to Cart</p> 
                 </div>
@@ -75,7 +82,66 @@ const populateContainer = (dataArr) => {
   }
 };
 
+const verifyCart = (e) => {
+  let i = btnArr.findIndex((btn) => btn == e.target);
 
+  for (let dessert of dessertInCart) {
+    if (dessert.dIndex == i) {
+      return incrementQty(i);
+    }
+  }
+
+  getDataTo(modifyCartList, i);
+  //executar função de incluir sobremesa no carrinho
+};
+
+const incrementQty = (i) => {};
+
+const modifyCartList = (dataArr, i) => {
+  if (!dataArr) {
+    console.log("data not founded");
+    return;
+  }
+
+  if (i < 0) {
+    console.log("index not referenced");
+    return;
+  }
+
+  let { name, price } = dataArr[i];
+
+  dessertInCart.push({
+    dIndex: i,
+    price,
+    name,
+    qty: 1,
+  });
+
+  let { dIndex, qty } = dessertInCart.find((dessert) => dessert.name === name);
+
+  wrappersArray[i].classList.add("clicked");
+  wrappersArray[i].innerHTML = `
+    <button class="minus_button"> ${icons.minus} </button>
+    <p id="qty_${dIndex}"> ${qty} </p> 
+    <button class="plus_button"> ${icons.plus} </button> 
+  `;
+
+  cartList.innerHTML = "";
+  for (let dessert of dessertInCart) {
+    let { dIndex, name, qty, price } = dessert;
+    let totalPrice = Number(qty * price).toFixed(2);
+    cartList.innerHTML += `
+      <section class='order ${dIndex}'>
+        <span class='order_name'>${name}</span>
+        <div class='price_wrapper'>
+          <p class='order_qty'> ${qty}x</p>
+          <p class='unity_price'> @${price} </p>
+          <p class='total_order_price'> $${totalPrice}
+        </div>
+      </section>
+    `;
+  }
+};
 
 window.onload = () => {
   if (cartList.innerHTML == "") {
